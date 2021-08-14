2001ecb40245be8c77e6eda6bda937f693d4e931
tayyab212 <tthunjra@gmail.com>
2021-08-14 23:12:37 +0500

    minor

--- Models/CameraDetail.cs
+++ Models/CameraDetail.cs
@@ -11,6 +11,7 @@ namespace CameraDesription.Models
         public string Name { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
+        public int Index { get; set; }
     }
 
     public class CameraDetailList
